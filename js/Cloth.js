9b6be96bdf27cf724c47681cbcae90d151ec6656
filipecn <fuiripecn@gmail.com>
2014-03-27 10:48:14 -0300

    cloth!

---
+++ js/Cloth.js
@@ -0,0 +1,194 @@
+var CLOTH = {};
+
+CLOTH.data = function(){
+	return {
+			cur: [],
+			last: [],
+			swap: function() {
+					var tmp = this.cur;
+					this.cur = this.last;
+					this.last = tmp;
+				},
+			print: function() {
+				console.log("last " + this.last);
+				console.log("cur " + this.cur);
+			}
+		};
+};
+
+CLOTH.Constraint = function(a, b, r){
+	this.a = a;
+	this.b = b;
+	this.restlength = r;
+};
+
+CLOTH.Cloth = function(scene){
+	//CLOTH PARAMETERS
+	this.L = 10;
+	this.NW = 20;
+	this.NH = 20;
+	var geometry = new THREE.Geometry();
+	for(var x = 0; x < this.NW; x++)
+		for(var z = 0; z < this.NH; z++){
+			geometry.vertices.push(new THREE.Vector3(x*this.L, 150, z*this.L))
+		}
+
+	//SIMULATION DATA
+	this.N = geometry.vertices.length;
+	this.X = CLOTH.data();
+	this.F = CLOTH.data();
+	this.M = CLOTH.data();
+	this.g = new THREE.Vector3(0,-10.0,0);
+	this.timeStep = 0.1;
+	for(var i = 0; i < this.N; i++){
+		this.F.cur[i] = this.g.clone();
+		this.X.cur[i] = geometry.vertices[i].clone();
+		this.X.last[i] = geometry.vertices[i].clone();
+		this.M.cur[i] = i*0.5;
+	}
+	this.iterationsNumber = 3;
+	this.constraints = [];
+	for(var i = 0; i < this.NW; i++)
+		for(var j = 0; j < this.NH; j++){
+			var d = 1;
+			if(i < this.NW - d)
+				this.constraints.push(new CLOTH.Constraint(j*this.NW + i,j*this.NW + i + d,d*this.L));
+			if(j < this.NH - d)
+				this.constraints.push(new CLOTH.Constraint(j*this.NW + i,(j+d)*this.NW + i,d*this.L));
+			if(i < this.NW - d && j < this.NH - d)
+				this.constraints.push(new CLOTH.Constraint(j*this.NW + i,(j+d)*this.NW + i + d,d*this.L*Math.sqrt(2.0)));
+			if(i >= 0 + d && j < this.NH - d)
+				this.constraints.push(new CLOTH.Constraint(j*this.NW + i,(j+d)*this.NW + i - d,d*this.L*Math.sqrt(2.0)));
+				
+		}
+
+	this.simulationStep = 0;
+
+	var discTexture = THREE.ImageUtils.loadTexture( 'images/disc.png' );
+
+	// values that are constant for all particles during a draw call
+	var customUniforms = 
+	{
+		texture:   { type: "t", value: discTexture },
+	};
+
+	// properties that may vary from particle to particle. only accessible in vertex shaders!
+	//	(can pass color info to fragment shader via vColor.)
+	var customAttributes = 
+	{
+		customColor:   { type: "c", value: [] },
+	};
+
+	// assign values to attributes, one for each vertex of the geometry
+	for( var v = 0; v < geometry.vertices.length; v++ ) 
+	{
+		customAttributes.customColor.value[ v ] = new THREE.Color( 0xffffff * Math.random() );
+	}
+
+	var shaderMaterial = new THREE.ShaderMaterial( 
+	{
+		uniforms: 		customUniforms,
+		attributes:		customAttributes,
+		vertexShader:   document.getElementById( 'vertexshader' ).textContent,
+		fragmentShader: document.getElementById( 'fragmentshader' ).textContent,
+		transparent: true, alphaTest: 0.5
+	});
+
+	
+	this.points = new THREE.ParticleSystem( geometry, shaderMaterial );
+	this.points.position.set(0, 0, 0);
+	this.points.dynamic = true;
+	this.points.sortParticles = true;
+	scene.add( this.points );
+
+	// FUN STUFF
+	var geometry = new THREE.SphereGeometry( 20, 32, 32 );
+	var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+	this.sphere = new THREE.Mesh( geometry, material );
+	scene.add( this.sphere );
+};
+
+CLOTH.Cloth.prototype.verlet = function(){
+	for(var i = 0; i < this.N; i++){
+		var tmp = this.X.cur[i].clone();
+		this.X.cur[i].x += this.X.cur[i].x-this.X.last[i].x+this.F.cur[i].x*this.timeStep*this.timeStep;
+		this.X.cur[i].y += this.X.cur[i].y-this.X.last[i].y+this.F.cur[i].y*this.timeStep*this.timeStep;
+		this.X.cur[i].z += this.X.cur[i].z-this.X.last[i].z+this.F.cur[i].z*this.timeStep*this.timeStep;
+        this.X.last[i].copy(tmp);
+	}
+};
+
+CLOTH.Cloth.prototype.accumulateForces = function(){
+	for(var i = 0; i < this.N; i++){
+		this.F.cur[i].copy(this.g);
+	}
+};
+
+CLOTH.Cloth.prototype.satisfyConstraints = function(){
+	for(var it = 0; it < this.iterationsNumber; it++){
+		for(var i = 0; i < this.N; i++){
+			this.X.cur[i].clamp(new THREE.Vector3(-50,-50,-50), new THREE.Vector3(200,200,200));
+		}
+
+		for(var i = 0; i < this.constraints.length; i++){
+			var x1 = this.X.cur[this.constraints[i].a].clone();
+			var x2 = this.X.cur[this.constraints[i].b].clone();
+			var r = this.constraints[i].restlength;
+			var delta = new THREE.Vector3(x2.x - x1.x,x2.y - x1.y,x2.z - x1.z);
+
+			var deltalength = Math.sqrt(delta.dot(delta));
+			var diff = (deltalength-r)/(deltalength);
+			this.X.cur[this.constraints[i].a].x += 0.5*delta.x*diff;
+			this.X.cur[this.constraints[i].a].y += 0.5*delta.y*diff;
+			this.X.cur[this.constraints[i].a].z += 0.5*delta.z*diff;
+
+			this.X.cur[this.constraints[i].b].x -= 0.5*delta.x*diff;
+			this.X.cur[this.constraints[i].b].y -= 0.5*delta.y*diff;
+			this.X.cur[this.constraints[i].b].z -= 0.5*delta.z*diff;
+		}
+
+		for(var i = 0; i < this.N; i++){
+			var delta = this.X.cur[i].clone();
+			delta.sub(this.sphere.position);
+			//console.log(delta.dot(delta) + " " + this.sphere.geometry.radius*this.sphere.geometry.radius );
+			if(delta.dot(delta) <= this.sphere.geometry.radius*this.sphere.geometry.radius){
+				//console.log("c!");
+				delta.setLength(this.sphere.geometry.radius);
+				delta.add(this.sphere.position);
+				this.X.cur[i].copy(delta);
+			}
+		}
+
+		this.X.cur[0].set(0,150,0);
+		this.X.cur[this.NW-1].set(0,150,(this.NH-1)*this.L);
+		this.X.cur[(this.NH-1)*this.NW].set((this.NW-1)*this.L,150,0);
+		this.X.cur[this.NH*this.NW - 1].set((this.NW-1)*this.L,150,(this.NH-1)*this.L);
+		
+	}
+};
+
+CLOTH.Cloth.prototype.update = function(){
+	var t = this.simulationStep/70;
+	this.simulationStep++;
+
+	this.sphere.position = new THREE.Vector3(
+			20.0 * Math.cos(2.0 * t) * (3.0 + Math.cos(3.0 * t)),
+			20.0 * Math.sin(2.0 * t) * (3.0 + Math.cos(3.0 * t)),
+			50.0 * Math.sin(3.0 * t) );
+	this.sphere.position.add(new THREE.Vector3(100,100,100));
+
+	this.accumulateForces();
+	this.verlet();
+	this.satisfyConstraints();
+	//console.log("antes");
+	//console.log(this.points.geometry.vertices[0]);
+	//console.log(this.points.geometry.vertices[1]);
+	//console.log("depois");
+	for(var i = 0; i < this.N; i++){
+		this.points.geometry.vertices[i].copy(this.X.cur[i]);
+	//	console.log(this.points.geometry.vertices[i]);
+	}
+	this.points.geometry.verticesNeedUpdate = true;
+};
+
+//CLOTH.Cloth.update.addContraint(t)
